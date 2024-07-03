10470ed491ac5e86539048d2feff621efc26b7c5
ThuyDuong <httd343@gmail.com>
2024-07-03 17:27:53 +0700

    add: firebase setting

---
+++ src/firebase.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBQNkYY2K1FjlQH6-w0Q04fzW_esJbSK2E",
+  authDomain: "twitter-reloaded-67572.firebaseapp.com",
+  projectId: "twitter-reloaded-67572",
+  storageBucket: "twitter-reloaded-67572.appspot.com",
+  messagingSenderId: "1077327019497",
+  appId: "1:1077327019497:web:736cdd299da7c993815f78"
+};
+
+
+const app = initializeApp(firebaseConfig);
