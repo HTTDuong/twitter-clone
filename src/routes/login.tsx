336f7fa7f21da47cdf159c81d28fb882ee1ce7bb
ThuyDuong <httd343@gmail.com>
2024-07-03 22:02:29 +0700

    add: register

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,7 +1,89 @@
-export default function Login() {
+import { useState } from "react";
+import { FaXTwitter } from "react-icons/fa6";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase";
+import { Link, useNavigate } from "react-router-dom";
+import { FirebaseError } from "firebase/app";
+import {
+  Form,
+  Input,
+  Switcher,
+  Title,
+  Wrapper,
+  Error,
+} from "../components/auth-component";
+
+export default function LogIn() {
+  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { name, value },
+    } = e;
+    switch (name) {
+      case "email":
+        setEmail(value);
+        break;
+      case "password":
+        setPassword(value);
+        break;
+    }
+  };
+
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setError("");
+    if (isLoading || email === "" || password === "") {
+      return;
+    }
+    try {
+      setIsLoading(true);
+      await signInWithEmailAndPassword(auth, email, password);
+      navigate("/");
+    } catch (err) {
+      if (err instanceof FirebaseError) {
+        setError(err.message);
+      } else {
+        setError(JSON.stringify(err));
+      }
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <Wrapper>
+      <Title>
+        Log into <FaXTwitter />
+      </Title>
+      <Form onSubmit={onSubmit}>
+        <Input
+          onChange={onChange}
+          name="email"
+          value={email}
+          placeholder="Email"
+          type="email"
+          required
+        />
+        <Input
+          onChange={onChange}
+          name="password"
+          value={password}
+          placeholder="Password"
+          type="password"
+          required
+        />
+        <Input type="submit" value={isLoading ? "Loading..." : "Log in"} />
+      </Form>
+      {error && <Error>{error}</Error>}
+      <Switcher>
+        Don't have an account?{" "}
+        <Link to="/create-account">Create one &rarr;</Link>
+      </Switcher>
+    </Wrapper>
   );
 }
